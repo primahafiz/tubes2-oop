64e0d1933c84d9c1645c3be74f75c6ce5f398fc8
primahafiz <primahafiz@gmail.com>
2022-04-15 13:26:27 +0700

    janlup make jdk17

---
+++ src/main/java/com/gui/mainguiwindow/HelloApplication.java
@@ -0,0 +1,26 @@
+package com.gui.mainguiwindow;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        fxmlLoader.setController(new HelloController()); //kalau mau tambahin params
+
+        Scene scene = new Scene(fxmlLoader.load(), 960, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
