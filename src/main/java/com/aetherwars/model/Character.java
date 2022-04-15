cc8cad9516a3becd7260eae68069884b6e3d62f4
Claudia <88203931+clauculus@users.noreply.github.com>
2022-04-15 12:44:04 +0700

    update char

--- src/main/java/com/aetherwars/model/Character.java
+++ src/main/java/com/aetherwars/model/Character.java
@@ -6,6 +6,8 @@ public class Character extends Card{
   private int health;
   private int attackUp;
   private int healthUp;
+  private int exp;
+  private int level;
 
 
   public Character(int id, String name, String desc, String path, int mana, Type type, int attack, int health, int attackUp, int healthUp) {
@@ -15,6 +17,8 @@ public class Character extends Card{
     this.health = health;
     this.attackUp = attackUp;
     this.healthUp = healthUp;
+    this.exp = 0;
+    this.level = 1;
   }
 
   // getter
@@ -38,6 +42,56 @@ public class Character extends Card{
   public int getHealthUp(){
     return this.healthUp;
   }
+
+  public int getExp(){
+    return this.exp;
+  }
+
+  public int getLevel(){
+    return this.level;
+  }
+
+  private boolean isStrongerThan(Character c){
+    boolean stronger = false;
+
+    if ((this.getType() == Type.END && c.getType() == Type.NETHER) ||
+        (this.getType() == Type.NETHER && c.getType() == Type.OVERWORLD) ||
+        (this.getType() == Type.OVERWORLD && c.getType() == Type.END)) {
+      stronger = true;
+    }
+
+    return stronger;
+  }
+  private boolean isEqual(Character c){
+    return this.getType() == c.getType();
+  }
+
+  public double getDamage(Character c){
+    double damage;
+
+    if (this.isStrongerThan(c)){
+      damage = 2 * attack;
+    } else if (this.isEqual(c)) {
+      damage = attack;
+    } else {
+      damage =  0.5 * attack;
+    }
+
+    return damage;
+  }
+
+  public void addExp(int exp){
+    this.exp += exp;
+  }
+
+  public void setHealth(int health){
+    this.health = health;
+  }
+
+  public boolean isDead(){
+    return this.health == 0;
+  }
+
 
   /*
   @Override
