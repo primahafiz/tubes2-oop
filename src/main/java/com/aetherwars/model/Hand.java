cd71871414c7c15ac2165d368f5f62b0544d5ea6
lyorafelicya <16520164@std.stei.itb.ac.id>
2022-04-19 05:17:00 +0700

    modif hand

--- src/main/java/com/aetherwars/model/Hand.java
+++ src/main/java/com/aetherwars/model/Hand.java
@@ -2,11 +2,37 @@ package com.aetherwars.model;
 import com.aetherwars.model.Card.*;
 import java.util.*;
 public class Hand {
-    private final List<Card> Hand = new ArrayList<Card>();
-    public Hand{
-        int i;
-        for (i = 0; i < 5; i++){
-            this.Hand.add(getCard());
-        }
+
+    private List<Card> onHandCards;
+
+    public Hand() {
+        this.onHandCards = new ArrayList<Card>(5);
     }
+
+    public void removeCardfromHand(int idx) {
+        this.onHandCards.remove(idx);
+    }
+
+    public boolean isEmpty() {
+        return (this.onHandCards.size() == 0);
+    }
+
+    public boolean isFull() {
+        return (this.onHandCards.size() == 5);
+    }
+
+    public int numberOfCards() {
+        return (this.onHandCards.size());
+    }
+
+    public Card getCard(int id) {
+        return this.onHandCards.get(id);
+    }
+//    private final List<Card> Hand = new ArrayList<Card>();
+//    public Hand{
+//        int i;
+//        for (i = 0; i < 5; i++){
+//            this.Hand.add(getCard());
+//        }
+//    }
 }
