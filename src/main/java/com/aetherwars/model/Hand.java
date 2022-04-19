427224b73ec7bbcb08b23564a8f651f8b9e62ea5
Nicholass12 <13520121@std.stei.itb.ac.id>
2022-04-19 22:17:16 +0700

    hand & deck

--- src/main/java/com/aetherwars/model/Hand.java
+++ src/main/java/com/aetherwars/model/Hand.java
@@ -2,14 +2,12 @@ package com.aetherwars.model;
 import com.aetherwars.model.Card.*;
 import java.util.*;
 public class Hand {
-
-    private List<Card> onHandCards;
-
+    private final List<Card> onHandCards;
     public Hand() {
         this.onHandCards = new ArrayList<Card>(5);
     }
-
-    public void removeCardfromHand(int idx) {
+    
+    public void removeCardFromHand(int idx) {
         this.onHandCards.remove(idx);
     }
 
@@ -28,11 +26,4 @@ public class Hand {
     public Card getCard(int id) {
         return this.onHandCards.get(id);
     }
-//    private final List<Card> Hand = new ArrayList<Card>();
-//    public Hand{
-//        int i;
-//        for (i = 0; i < 5; i++){
-//            this.Hand.add(getCard());
-//        }
-//    }
 }
