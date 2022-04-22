4deb114ff0d055088a35b12d00a981ad015ccf59
Nicholass12 <13520121@std.stei.itb.ac.id>
2022-04-22 21:10:28 +0700

    deck

--- src/main/java/com/aetherwars/AetherWars.java
+++ src/main/java/com/aetherwars/AetherWars.java
@@ -3,36 +3,42 @@ package com.aetherwars;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
+import java.util.*;
 
-import com.aetherwars.model.Type;
+import com.aetherwars.model.*;
+import com.aetherwars.model.Deck;
 import com.aetherwars.model.Character;
-import com.aetherwars.model.Card;
 import com.aetherwars.util.CSVReader;
-import com.aetherwars.model.CardReader;
 
 public class AetherWars {
 
 
   public static void main(String[] args) {
+//
+//    try {
+//      System.out.println("Hello World");
+//      CardReader c = new CardReader(); // ctor
+//      c.addAllCards(); // masukin card ke c
+//      int i = 0;
+//      for (Card card : c.getCards()){
+//        i += 1;
+//        System.out.println("Kartu ke-" + i);
+//        card.printCardInfo(); // print setiap card
+//        System.out.println();
+//      }
+//
+//      // kalo mau debug di sini
+//
+//    } catch (Exception e){
+//
+//    }
 
-    try {
-      System.out.println("Hello World");
-      CardReader c = new CardReader(); // ctor
-      c.addAllCards(); // masukin card ke c
-      int i = 0;
-      for (Card card : c.getCards()){
-        i += 1;
-        System.out.println("Kartu ke-" + i);
-        card.printCardInfo(); // print setiap card
+    int i;
+    List<Card> c = new Deck().getAll();
+    for (i = 0;i < c.size();i++){
+        System.out.println("Kartu ke-" + (i+1));
+        c.get(i).printCardInfo();
         System.out.println();
-      }
-
-      // kalo mau debug di sini
-
-    } catch (Exception e){
-
-    }
-
   }
+}
 }
--- src/main/java/com/aetherwars/model/Deck.java
+++ src/main/java/com/aetherwars/model/Deck.java
@@ -2,14 +2,21 @@ package com.aetherwars.model;
 import java.util.*;
 public class Deck {
 
-    private final List<Card> CardDeck= new ArrayList<Card>();
+    private List<Card> CardDeck= new ArrayList<Card>();
 
-    public Deck(){
-        Random rand = new Random();
-        int capacity = rand.nextInt(20) + 40;
-        int i;
-        for (i = 0; i < capacity; i++){
-            //this.CardDeck.add();
+    public Deck() {
+        try{
+            Random rand = new Random();
+            int capacity = rand.nextInt(20) + 40;
+            int i;
+            CardReader c = new CardReader();
+            c.addAllCards();
+            for (i = 0;i < capacity; i++){
+                int random = rand.nextInt(c.getCards().size());
+                CardDeck.add(c.getCards().get(random));
+            }
+        } catch (Exception e) {
+
         }
     }
 
@@ -21,5 +28,9 @@ public class Deck {
              this.CardDeck.remove(i);
         }
         return C;
+    }
+
+    public List<Card> getAll(){
+        return this.CardDeck;
     }
 }
